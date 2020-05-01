1c5f5a71b1076dd678728f83e527ad2c1f745a84
Ricardo Russ <ricardoruss@live.com>
2020-05-01 00:01:09 -0400

    added landing page

--- employee-tracker/src/App.js
+++ employee-tracker/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './components/employeedirectory'
 import NavBar from "./components/all/NavBar";
 import EmployeeDirectory from './components/employeedirectory';
+import landing from './containers/landing'
 
 function App() {
   return (
     <Router>
       <NavBar/>
-   <Route exact path="/" component={EmployeeDirectory} />
+   <Route exact path="/" component={landing} />
 </Router>
   );
 }
---
+++ employee-tracker/src/containers/landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const landing = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div class="col">
+                    <h1>Welcome to the Employee Tracker!</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default landing;
