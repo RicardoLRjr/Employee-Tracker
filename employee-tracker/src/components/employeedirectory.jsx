0329a9dac9b7e16349e4bbf76c94a764e3f4b330
Ricardo Russ <ricardoruss@live.com>
2020-04-30 22:52:23 -0400

    removed unneeded legacy coded

---
+++ employee-tracker/src/components/employeedirectory.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EmployeeDirectory extends Component {
+    render() {
+        return (
+            <div>
+                hello world!
+            </div>
+        );
+    }
+}
+
+export default EmployeeDirectory;
